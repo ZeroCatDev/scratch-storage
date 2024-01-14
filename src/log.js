2f6a4df00b29c03b95a86d0a3f9a5ccaafad1c4a
Muffin <muffin@mailbox.org>
2024-01-14 02:00:13 -0600

    Replace minilog with nanolog

--- src/log.js
+++ src/log.js
@@ -1,4 +1,4 @@
-const minilog = require('minilog');
-minilog.enable();
+const nanolog = require('@turbowarp/nanolog');
+nanolog.enable();
 
-module.exports = minilog('storage');
+module.exports = nanolog('storage');
